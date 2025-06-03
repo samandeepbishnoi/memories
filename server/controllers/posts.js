ab2ec0cebdec16063993dc059bba2b309d7cad72
samandeepbishnoi <samandeepbishnoi@gamil.com>
2025-06-03 13:34:35 +0530

    Refactor posts controller and model to implement getPost and createPost functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,34 @@
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = (req, res) => {
-    res.send('This is the posts route');
-}
-export const createPost = (req, res) => {
-    res.send('Post created successfully');
-} 
+
+export const getPost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await PostMessage.findById(id);
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPostMessage = new PostMessage({
+    ...post,
+    creator: req.userId,
+    createdAt: new Date().toISOString(),
+  });
+
+  try {
+    await newPostMessage.save();
+
+    res.status(201).json(newPostMessage);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+ 
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,14 +1,23 @@
-import mongoose from 'mongoose';
-const postSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    message: { type: String, required: true },
-    creator: { type: String, required: true },
-    tags: { type: [String]},
-    selectedFile: { type: String },
-    likeCount: { type: Number, default: 0 },
-    createdAt: { type: Date, default: new Date() },
-
-})
-
+import mongoose from "mongoose";
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    name: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likes:{
+        type: [String],
+        default: [],
+    },
+    comments: {
+        type: [String],
+        default: [],
+    },
+    createdAt:{
+        type: Date,
+        default: new Date(),
+    }
+});
 const PostMessage = mongoose.model('PostMessage', postSchema);
-export default PostMessage; 
+export default PostMessage;
