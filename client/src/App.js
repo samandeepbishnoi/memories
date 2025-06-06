b556fb65b57b4aa335c2460b1f5a79b5b4dfa587
samandeepbishnoi <samandeepbishnoi@gamil.com>
2025-06-06 16:37:47 +0530

    feat: update routing in App component to include navigation and user authentication handling

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,25 @@ import { Container  } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import PostDetails from './components/PostDetails/PostDetails';
+import { BrowserRouter, Routes , Route ,Navigate} from 'react-router-dom';
 
 const App = () => {
-   
+   const user = JSON.parse(localStorage.getItem('profile'));
     return (
     
         <Container maxWidth="lg">
            <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} /> 
+          <Route path="/" exact Component={() => <Navigate to="/posts" />} />
+            <Route path="/posts" exact Component={Home} />
+            <Route path="/posts/search" exact Component={Home} />
+            <Route path="/posts/:id" exact Component={PostDetails} />
+            <Route
+              path="/auth"
+              exact
+              Component={() => (!user ? <Auth /> : <Navigate to="/posts" />)}
+            />
         </Routes>
             
         </Container>
