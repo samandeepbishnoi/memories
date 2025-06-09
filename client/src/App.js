14e92df85065937324143a3e716b222936dcf30d
samandeepbishnoi <samandeepbishnoi@gamil.com>
2025-06-09 21:42:11 +0530

    refactor: clean up App component; standardize JSX formatting and improve readability

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,31 @@
-import React  from 'react';
-import { Container  } from '@material-ui/core';
+import React from 'react';
+import { Container } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import PostDetails from './components/PostDetails/PostDetails';
-import { BrowserRouter, Routes , Route ,Navigate} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
-   const user = JSON.parse(localStorage.getItem('profile'));
-    return (
+  const user = JSON.parse(localStorage.getItem("profile"));
+  const isLoggedIn = user && user?.token;
+
+
+  return (
     
-        <Container maxWidth="xl">
-           <Navbar />
+      <Container maxWidth="xl">
+        <Navbar />
         <Routes>
-          <Route path="/" exact Component={() => <Navigate to="/posts" />} />
-            <Route path="/posts" exact Component={Home} />
-            <Route path="/posts/search" exact Component={Home} />
-            <Route path="/posts/:id" exact Component={PostDetails} />
-            <Route
-              path="/auth"
-              exact
-              Component={() => (!user ? <Auth /> : <Navigate to="/posts" />)}
-            />
+          <Route path="/" element={<Navigate to="/posts" />} />
+          <Route path="/posts" element={<Home />} />
+          <Route path="/posts/search" element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/auth" element={!isLoggedIn ? <Auth /> : <Navigate to="/posts" />} />
+
         </Routes>
-            
-        </Container>
-       
-    );
-}
+      </Container>
+    
+  );
+};
 
 export default App;
